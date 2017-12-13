0ec8409d3f03dd85045268a7e9bda05af2c26f71
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2017-12-13 20:24:17 +0200

    Initializes Unity project and adds assets

---
+++ Assets/Background Stuff/Background/ResponsiveCamera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponsiveCamera : MonoBehaviour
+{
+
+    public Vector2 EmulatedResolution = new Vector2(272, 160);
+	
+	void Awake ()
+	{
+	    var camera  = GetComponent<Camera>();
+        var screenAspectRatio = (float) Screen.width / Screen.height;
+        Debug.Log(screenAspectRatio);
+
+        if (screenAspectRatio > 16.0f / 9.0f)
+        {
+
+            camera.orthographicSize = (EmulatedResolution.x / screenAspectRatio) / 2;
+
+        }
+	    else
+	    {
+	        camera.orthographicSize = EmulatedResolution.y / 2;
+        }
+	}
+}
