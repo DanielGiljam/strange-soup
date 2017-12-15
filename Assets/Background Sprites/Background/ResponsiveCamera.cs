4f9aa4a91d69469cfe6dbffd7835457ab3d9d4cd
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2017-12-15 15:22:45 +0200

    WIP 2017-12-15

--- Assets/Background Sprites/Background/ResponsiveCamera.cs
+++ Assets/Background Sprites/Background/ResponsiveCamera.cs
@@ -6,23 +6,25 @@ using UnityEngine;
 public class ResponsiveCamera : MonoBehaviour
 {
 
-    public Vector2 EmulatedResolution = new Vector2(272, 160);
+    public Vector2 EmulatedResolution = new Vector2(17, 10);
 	
 	void Awake ()
 	{
-	    var camera  = GetComponent<Camera>();
-        var screenAspectRatio = (float) Screen.width / Screen.height;
-        Debug.Log(screenAspectRatio);
+        Camera cam  = GetComponent<Camera>();
+        float screenAspectRatio = (float) Screen.width / Screen.height;
+        Debug.Log("Screeen width: " + Screen.width);
+        Debug.Log("Screeen height: " + Screen.height);
+        Debug.Log("Screen aspect ratio --> " + screenAspectRatio);
+        Debug.Log("True 16:9 --> " + 16.0f / 9.0f);
 
-        if (screenAspectRatio > 16.0f / 9.0f)
+        if (Mathf.Ceil(screenAspectRatio * 100) > 177)
         {
-
-            camera.orthographicSize = (EmulatedResolution.x / screenAspectRatio) / 2;
-
+            cam.orthographicSize = (EmulatedResolution.x / (16.0f / 9.0f)) / 2;
         }
 	    else
 	    {
-	        camera.orthographicSize = EmulatedResolution.y / 2;
+	        cam.orthographicSize = EmulatedResolution.y / 2;
         }
 	}
+
 }
---
+++ Assets/Character Controls/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float movementMultiplier = 5;
+
+    Rigidbody2D rb;
+    float hInputValue;
+    float hVel;
+    float vVel;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        hInputValue = Input.GetAxis("Horizontal");
+        hVel = hInputValue * movementMultiplier;
+        vVel = rb.velocity.y;
+        rb.velocity = new Vector2(hVel, vVel);
+	}
+
+}
