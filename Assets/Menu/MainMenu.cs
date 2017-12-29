37a4c48c3b1b9074ff558b8f475660e91eb63483
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2017-12-29 14:18:09 +0200

    Adds WIP menu

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Menu
+{
+    public class MainMenu : MonoBehaviour {
+
+        public void Play()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void Quit()
+        {
+            Debug.Log("QUIT");
+            Application.Quit();
+        }
+
+    }
+}
